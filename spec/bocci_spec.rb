d7104bf25c10530ea699a55288cb6ae123c31d1d
mrpero <abe@zikuu.jp>
2017-04-26 13:52:09 +0900

    test release

--- spec/bocci_spec.rb
+++ spec/bocci_spec.rb
@@ -1,11 +1,19 @@
 require "spec_helper"
 
 RSpec.describe Bocci do
-  it "has a version number" do
-    expect(Bocci::VERSION).not_to be nil
-  end
+	
+	it "has a version number" do
+		expect(Bocci::VERSION).not_to be nil
+	end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+	it "does something useful" do
+		expect(false).to eq(true)
+	end
+
+	describe '#greet' do
+		it 'returns "Hello World!"' do
+			expect(TestGem.greet).to eq('Hello World!')
+		end
+	end
+
 end
